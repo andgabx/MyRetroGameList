e5df653df3d14cb2eae6316a7f3825cbb0d7f928
Filipe Macedo <lfms@cesar.school>
2024-09-29 18:21:21 -0300

    Update settings.py

--- myretrogamelist/settings.py
+++ myretrogamelist/settings.py
@@ -137,3 +137,8 @@ STATICFILES_STORAGE = ('whitenoise.storage.CompressedManifestStaticFilesStorage'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTHENTICATION_BACKENDS = (
+    'django.contrib.auth.backends.ModelBackend',  # Default backend
+    'app.auth.AuthBackend',  # Your custom backend
+)
