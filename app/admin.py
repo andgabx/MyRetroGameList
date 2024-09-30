f36255eb173c1241218e0a8c35036fcf171a6d16
filipe-ms <lfms@cesar.school>
2024-09-30 13:26:01 -0300

    Another Fix

--- app/admin.py
+++ app/admin.py
@@ -3,4 +3,7 @@ from django.contrib.auth.admin import UserAdmin
 from django.contrib.auth.models import User
 from .models import Profile, Game
 
-admin.site.register(User, Profile, UserAdmin, Game)
+admin.site.unregister(User)
+admin.site.register(User, UserAdmin)
+admin.site.register(Profile)
+admin.site.register(Game)
