98c0b3611197b47dd6a94ff98661430b9871c65e
filipe-ms <lfms@cesar.school>
2024-09-30 13:02:14 -0300

    Reverting the extended AbstractUser Model (forgot one in settings.py)

--- myretrogamelist/settings.py
+++ myretrogamelist/settings.py
@@ -140,5 +140,3 @@ STATICFILES_STORAGE = ('whitenoise.storage.CompressedManifestStaticFilesStorage'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-AUTH_USER_MODEL = 'app.models.User'
