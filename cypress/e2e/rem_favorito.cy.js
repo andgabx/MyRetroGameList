1328a49ad882bf078611e1802ab7255a0e521775
Yuri Cavalcanti <ycs@cesar.school>
2024-10-23 15:51:16 -0300

    testes e2e intalando cypress5

--- cypress/e2e/rem_favorito.cy.js
+++ cypress/e2e/rem_favorito.cy.js
@@ -8,12 +8,12 @@ describe('template spec', () => {
     cy.get('[type="password"]').type(password)
     cy.get('[type="submit"]').click()
     cy.visit('http://127.0.0.1:8000/gamelist/')
-    cy.get(':nth-child(3) > .flex-grow > .justify-between > .flex > .px-2 > .bi').click()
+    cy.get(':nth-child(2) > .flex-grow > .justify-between > .flex > .px-2 > .bi').click()
   })
   
   it('teste_cadastro', () => {
     cy.visit('http://127.0.0.1:8000/gamelist/')
-    cy.get(':nth-child(3) > .flex-grow > .justify-between > .flex > .px-2 > .bi').click()
+    cy.get(':nth-child(2) > .flex-grow > .justify-between > .flex > .px-2 > .bi').click()
     cy.get('.w-12').click()
     cy.get(':nth-child(1) > .overflow-x-auto > .flex > .text-white').should('have.text','No games available.')
     
